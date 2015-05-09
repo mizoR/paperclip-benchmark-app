de130493dfbd021ec2141aa00bc964616af5dad4
mizokami <suzunatsu@yahoo.com>
2015-05-09 21:31:43 +0900

    Enable paperclip hash key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar,
     styles: { medium: "300x300>", thumb: "100x100>" },
-    default_url: "/images/:style/missing.png"
+    default_url: "/images/:style/missing.png",
+    url: "/system/:hash.:extension",
+    hash_secret: "785AFEE8C45E71086D262DB0B0A31442"
 
   validates_attachment_file_name :avatar,
     matches: [/png\Z/, /jpe?g\Z/]
