5ae3897448c4e04157607ce6dad5714cb85b189b
mizokami <suzunatsu@yahoo.com>
2015-05-09 21:02:27 +0900

    Seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+data = Hash.new { |hash, key|
+  login    = "users/#{key}"
+  password = "passwords/#{key}"
+  avatar   = File.open(Rails.root.join('data', 'Lenna.png'))
+
+  { login: login, password: password, avatar: avatar }
+}
+
+User.destroy_all
+
+(1..10).each do |i|
+  puts User.create!(data[i])
+end
