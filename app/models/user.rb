54efef96d9509bcd18853af0343c920e152f8bf2
mizokami <suzunatsu@yahoo.com>
2015-05-09 22:04:46 +0900

    Complicate url style

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar,
     styles: { medium: "300x300>", thumb: "100x100>" },
     default_url: "/images/:style/missing.png",
-    url: "/system/:hash.:extension",
+    url: "/system/:rails_env/:class/:attachment/:id/:hash.:extension",
     hash_secret: "785AFEE8C45E71086D262DB0B0A31442"
 
   validates_attachment_file_name :avatar,
